ea04601984579228b0ccb77b6a31e065151b5e56
zhangzhaohuan <zhangzhaohuan@ziggurat.cn>
2018-08-29 10:11:41 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import AC from './component/AC/AC';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <AC />
+      </div>
+    )
+  }
+}
